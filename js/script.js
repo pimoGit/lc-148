bfde81f520e590afa20299067fc9a72db3c54b2b
Simone <info@simoneicardi.com>
2025-08-26 12:07:30 +0200

    esempi primi usi variabili

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
-document.writeln('Hello World!');
-alert('Hello World!');
+// dichiarazione variabile
+let firstName;
 
-document.getElementById('mio_id').innerHTML =
-    'Hello World! EHEHEHE!';
+// assegnazione valore
+firstName = "Simone";
 
-/*
-commento
-multirigha
-sadas
-aszdx
-*/
+// dichiarazione e assegnazione
+const piGreco = 3.14;
 
-console.log("Hello World da console!!! di debug");
+// stringhe
+const userName = "Mario";
+const userSurname = 'Rossi';
+// versione concatenata
+const fullName = userName + " " + userSurname;
+// versione template literal
+const fullName2 = `Benvenuto ${userName} ${userSurname} 
+e buona giornata!`;
+console.log("il valore di fullName è: " + fullName);
+console.log("il valore di fullName2 è:", fullName2);
+
+// Numeri
+let numero1 = 7;
+let numero2 = 3;
+let numeroStringa = "dieci";
+
+let somma = numero1 + numero2;
+
+console.log("valore di somma", somma);
+
+let sommaSbagliata = somma + numeroStringa;
+
+console.log("valore di sommaSbagliata", sommaSbagliata);
+
+
+let prodotto = somma * numeroStringa;
+
+console.log("valore di prodotto", prodotto);
+
+
+
