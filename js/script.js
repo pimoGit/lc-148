3579f9a9b7627c8e5fdc7b2abe7209701687b7d6
Simone <info@simoneicardi.com>
2025-08-26 10:15:06 +0200

    primi es con JS

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.writeln('Hello World!');
+alert('Hello World!');
+
+document.getElementById('mio_id').innerHTML =
+    'Hello World! EHEHEHE!';
+
+/*
+commento
+multirigha
+sadas
+aszdx
+*/
+
+console.log("Hello World da console!!! di debug");
