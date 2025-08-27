185c5b5efa0fcc07734c0d352385ba3637b2254c
Simone <info@simoneicardi.com>
2025-08-27 11:31:56 +0200

    es. uso condizioni e logica con numeri

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
-// cricevendo in input due stringhe, andiamo a dire quale delle due è più lunga
+// ricevendo in input due età (numeri), andiamo a dire quale delle due è alta
 
 
 
 // PASSAGGI LOGICI:
 
+// inizializzazione variabili globali
 let messaggio;
 
 // input: prendiamo i valori delle due strighe
-const parola1 = prompt("inserisci la prima parola");
-const parola2 = prompt("inserisci la seconda parola");
+const eta1 = parseInt(prompt("inserisci la prima età"));
+const eta2 = parseInt(prompt("inserisci la seconda età"));
 
-console.log(parola1, parola2);
+
+
+console.log(eta1, eta2);
 
 
 
 // elaborazione: confrontiamo la lunghezza delle stringhe (maggiore, minore o uguale)
-if (parola1.length > parola2.length) {
-    messaggio = "parola 1 è più lunga";
-} else if (parola1.length < parola2.length) {
-    messaggio = "parola 2 è più lunga";
+if (eta1 > eta2) {
+    messaggio = "età 1 è più alta";
+} else if (eta1 < eta2) {
+    messaggio = "età 2 è più alta";
 } else {
-    messaggio = "le due parole sono lunghe uguali";
+    messaggio = "le due età sono uguali";
 }
 
 // output: che descrive quale parola sia più lunga
