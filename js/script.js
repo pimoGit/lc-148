c9759f82dfc65d4c398f0d09cbdfd043a72c2862
Simone <info@simoneicardi.com>
2025-08-27 10:56:07 +0200

    primo es. uso condizioni

--- js/script.js
+++ js/script.js
@@ -1,47 +1,27 @@
-// dichiarazione variabile
-let firstName, altraVar, nuovaVar;
-
-// assegnazione valore
-firstName = "Simone";
-
-// dichiarazione e assegnazione
-const piGreco = 3.14;
-
-// stringhe
-const userName = "Mario";
-const userSurname = 'Rossi';
-// versione concatenata
-const fullName = userName + " " + userSurname;
-// versione template literal
-const fullName2 = `Benvenuto ${userName} ${userSurname} 
-e buona giornata!`;
-console.log("il valore di fullName è: " + fullName);
-console.log("il valore di fullName2 è:", fullName2);
-
-// Numeri
-let numero1 = 7;
-let numero2 = 3;
-let numeroStringa = "dieci";
-
-let somma = numero1 + numero2;
-
-console.log("valore di somma", somma);
-
-let sommaSbagliata = somma + numeroStringa;
-
-console.log("valore di sommaSbagliata", sommaSbagliata);
-
-
-let prodotto = somma * numeroStringa;
-
-console.log("valore di prodotto", prodotto);
-
-// uso proprietà e metodi 
-const numeroDaFissare = 0.12345;
-const risultato = numeroDaFissare.toFixed(3);
-console.log(risultato);
-
-console.log(0.12345.toString());
+// cricevendo in input due stringhe, andiamo a dire quale delle due è più lunga
 
 
 
+// PASSAGGI LOGICI:
+
+let messaggio;
+
+// input: prendiamo i valori delle due strighe
+const parola1 = prompt("inserisci la prima parola");
+const parola2 = prompt("inserisci la seconda parola");
+
+console.log(parola1, parola2);
+
+
+
+// elaborazione: confrontiamo la lunghezza delle stringhe (maggiore, minore o uguale)
+if (parola1.length > parola2.length) {
+    messaggio = "parola 1 è più lunga";
+} else if (parola1.length < parola2.length) {
+    messaggio = "parola 2 è più lunga";
+} else {
+    messaggio = "le due parole sono lunghe uguali";
+}
+
+// output: che descrive quale parola sia più lunga
+console.log(messaggio);
