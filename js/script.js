6374f3f9cb2615eba4ef31af0c121bee9ce3ec5d
Simone <info@simoneicardi.com>
2025-08-29 11:31:04 +0200

    es. di traversing con condizione di array

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-const nomi = [1, 2, 3];
+// da array di numeri vogliamo stampare solo i numeri dispari
 
-for (let i = 0; i < nomi.length; i++) {
+// dichiarazione e valorizzazione dell'array
+const numbers = [10, 15, 2, 33, 55, 20, 22];
 
-    console.log("al numero di iterazione,e quindi alla posizione di array numero", i, "abbaimo: ", nomi[i]);
+// numbers.push(7, 12, 11, 91);
 
+
+// andiamo a traversare l'array
+for (let i = 0; i < numbers.length; i++) {
+    let num = numbers[i];
+
+    // andiamo a distinguere l'output per stampare solo i dispari
+    if (num % 2 !== 0) {
+        console.log(num);
+    }
 }
+
+
+numbers.push(7, 12, 11, 91);
+
