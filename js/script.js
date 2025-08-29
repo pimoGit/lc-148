2cc45b82af4e9ce80d0b35f2d33b8edf40379442
Simone <info@simoneicardi.com>
2025-08-29 12:24:32 +0200

    es. CRUD metodi array

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
-// da array di numeri vogliamo stampare solo i numeri dispari
+const cities = ['London', 'Shanghai', 'New York', 'Delhi', 'Kuala Lumpur'];
+const names = [];
+const numbers = [1, 2, 3];
+const colours = ['Red', 'Blue', 'Yellow'];
+const keys = ['q', 'w', 'e', 'r', 't', 'y'];
+const countries = ['Bolivia', 'Jordan', 'Greenland'];
+const fruits = ['Apple', 'Orange', 'Pear'];
 
-// dichiarazione e valorizzazione dell'array
-const numbers = [10, 15, 2, 33, 55, 20, 22];
+// 1. Aggiungi Fred all'array names
+names.push("Fred");
+console.log(names);
 
-// numbers.push(7, 12, 11, 91);
+// 2. Aggiungi 4 alla fine dell'array numbers
+numbers.push(4);
+console.log(numbers);
+
+// 3. Aggiungi Rio all'inizio dell'array cities
+cities.unshift("Rio");
+console.log(cities);
+
+// 4. Rimuovi il primo colore dall'array colours
+colours.shift();
+console.log(colours);
+
+// 5. Rimuovi l'ultimo elemento dall'array keys
+keys.pop();
+console.log(keys);
+
+// 6. Rimuovi l'ultimo frutto dall'array fruits e memorizzalo in pear qui sotto
+let pear = fruits.pop();
+console.log(fruits, "il valore estratto è: ", pear);
 
 
-// andiamo a traversare l'array
-for (let i = 0; i < numbers.length; i++) {
-    let num = numbers[i];
 
-    // andiamo a distinguere l'output per stampare solo i dispari
-    if (num % 2 !== 0) {
-        console.log(num);
-    }
+// 7. Rimuovi Jordan dall'array countries
+// mi ricavo l'indice dell'emento da cancellare
+const elementIndex = countries.indexOf('Jordan');
+console.log(elementIndex);
+// dopo un check su esistenza dell'emento lo cancello
+if (elementIndex > -1) { // only splice array when item is found
+    countries.splice(elementIndex, 1); // 2nd parameter means remove one item only
 }
 
+console.log(countries);
 
-numbers.push(7, 12, 11, 91);
+
+
+
+
 
