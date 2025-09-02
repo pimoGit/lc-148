c4a76244e2da6f3c4d08707a6d6e14ae7edebc45
Simone <info@simoneicardi.com>
2025-09-02 12:55:25 +0200

    altri esempi funzioni con stringhe

--- js/script.js
+++ js/script.js
@@ -9,11 +9,17 @@ console.log(result1, result2);
 
 console.log("esecuzione funzione randomNum:", randomNum(5, 20));
 
+const text1 = "qualsiasi";
+
+console.log(reverseWord(text1));
+
+const userWord = prompt("inserisci una parola") // Arrivederci
 
 
+// console.log(word1.toUpperCase());
 
-
-
+const parolaCapitalizzata = capitalizeWord(userWord);
+console.log("il risultato della funzione di capitalize è:", parolaCapitalizzata);
 
 
 
@@ -36,4 +42,43 @@ function checkPariDispari(numero) {
 // funzione di creazione numero random in un range
 function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// funzione di revert parola
+function reverseWord(word) {
+    // var di stringa revertata
+    let revertedWord = "";
+
+    // ciclo la stringa
+    for (let i = word.length - 1; i >= 0; i--) {
+        let letteraIesima = word[i];
+        // console.log(letteraIesima);
+        // revertedWord = revertedWord + letteraIesima;
+        revertedWord += letteraIesima;
+    }
+
+    // alternativa al FOR
+    // let revertedWord = word.split("").reverse().join("");
+
+    return revertedWord
+}
+
+// funzione che ritorna una stringa capitalizzata (solo prima lettera maiuscola)
+function capitalizeWord(word) {
+    // estraggo la prima lettera e  la rendo maiuscola
+    const firstLetter = word[0].toUpperCase();
+    // console.log("prima lettera da funzione di capitalize:", firstLetter);
+    // // la rendo maiuscola
+    // let firstLetterCap = firstLetter.toUpperCase();
+    // console.log("prima lettera da funzione di capitalize, maiuscola:", firstLetterCap);
+
+
+    // estraggo il resto della parola
+    const restWord = word.substring(1);
+
+    // compongo la stringa finale
+    const result = firstLetter + restWord;
+    // ritorno il valore composto
+    return result
+
 }
