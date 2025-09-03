77bdf1ff4b3e8e5d7eea841f251ef4d0d76416cd
Simone <info@simoneicardi.com>
2025-09-03 11:40:42 +0200

    es. di shorthand arrow funtions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,44 @@
-var a, b;
-
-
-
-a = 0;
-b = "buongiorno"
-var c = "ciao";
-
-saluta();
-
-console.log("a is " + a);
-console.log("b is " + b);
-
-function saluta() {
-    var d = "asd";
-    console.log("a is " + a);
-    var b = a + 1;
-    console.log("b is " + b);
-    console.log(c);
+// funzione di saluto
+function saluto() {
+    console.log("Ciao a tutti!");
 }
+
+
+
+
+// versione arrow
+const saluto1 = () => console.log("Ciao a tutti! arrow!!");
+
+console.log(saluto1);
+saluto1();
+
+saluto();
+
+
+// Funzione tradizionale
+function sum(a, b) {
+    return a + b;
+}
+
+// versione arrow
+const sum1 = (a, b) => {
+    return a + b;
+}
+
+// versione arrow compatta
+const sum2 = (a, b) => a + b;
+
+
+// Funzione tradizionale
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+// versione arrow
+const isEven1 = (number) => {
+    return number % 2 === 0;
+}
+
+// versione arrow compatta
+const isEven2 = number => number % 2 === 0;
+
