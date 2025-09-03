00f61050c285d09adaafe6c1feab59332a4d6694
Simone <info@simoneicardi.com>
2025-09-03 12:25:36 +0200

    es. di astrazione e creazione function

--- js/script.js
+++ js/script.js
@@ -1,44 +1,59 @@
-// funzione di saluto
-function saluto() {
-    console.log("Ciao a tutti!");
+// lista di invitati
+const invitati = ["invitato1", "invitato2", "invitato3", "invitato4"];
+
+// input utente da checkare
+const userCheck = prompt('inserisci la tua email');
+
+console.log("check array invitati:", isInArray(userCheck, invitati));
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+let myNumber = 7;
+
+console.log("check array numeri:", isInArray(8, numbers));
+
+
+
+/*
+// creo una variabile di stato (trovato vero/falso)
+let trovato = false;
+
+// cerchiamo
+for (let i = 0; i < invitati.length; i++) {
+
+    // variabile che contiene ad ogni iterazione un el dell'arr
+    let invitato = invitati[i];
+    console.log(invitato, i);
+
+    // faccio check di presenza invitato
+    if (invitato === userCheck) {
+        trovato = true;
+    }
+
 }
 
+console.log(trovato);
+*/
 
 
+// funzione di check elemnto in array
+function isInArray(inpuValue, arrayToCheck) {
+    // creo una variabile di stato (trovato vero/falso)
+    let trovato = false;
 
-// versione arrow
-const saluto1 = () => console.log("Ciao a tutti! arrow!!");
+    // cerchiamo
+    for (let i = 0; i < arrayToCheck.length; i++) {
 
-console.log(saluto1);
-saluto1();
+        // variabile che contiene ad ogni iterazione un el dell'arr
+        let arrayElement = arrayToCheck[i];
+        // console.log(arrayElement, i);
 
-saluto();
+        // faccio check di presenza invitato
+        if (arrayElement === inpuValue) {
+            trovato = true;
+        }
 
+    }
 
-// Funzione tradizionale
-function sum(a, b) {
-    return a + b;
+    return trovato
 }
-
-// versione arrow
-const sum1 = (a, b) => {
-    return a + b;
-}
-
-// versione arrow compatta
-const sum2 = (a, b) => a + b;
-
-
-// Funzione tradizionale
-function isEven(number) {
-    return number % 2 === 0;
-}
-
-// versione arrow
-const isEven1 = (number) => {
-    return number % 2 === 0;
-}
-
-// versione arrow compatta
-const isEven2 = number => number % 2 === 0;
-
