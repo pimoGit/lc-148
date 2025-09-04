3889fca4b9d1fb5721c3b73930d77b8c5b8feec0
Simone <info@simoneicardi.com>
2025-09-04 12:23:51 +0200

    es. base uso evento js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
-// chiediamo nome a User e diamo output colorato a seconda del sesso
-
 // SELEZIONE
-const outputEl = document.getElementById("output");
-console.log(outputEl);
+const mioParagrafo = document.querySelector(".myParagraph");
+console.log(mioParagrafo);
+
+const mioBottone = document.getElementById("myButton");
+console.log(mioBottone);
+
+// gestiamo evento su bottone
+mioBottone.addEventListener('click',
+    cambiaColore
+);
 
 
-// INPUT
-// chiediamo il nome allo User
-const nome = prompt("inserisci il tuo nome");
-
-// // chiediamo il sesso allo User
-const geneder = prompt("Sei unomo o donna? (M/F)");
-
-// vado a definire il colore del testo
-// let nameColor;
-// if (geneder === "M") {
-//     nameColor = "blue";
-// } else {
-//     nameColor = "pink";
-// }
-
-const nameColor = geneder === "M" ? 'blue' : "pink";
-
-
-// facciamo output come User
-outputEl.innerHTML = "Ciao " + nome;
-outputEl.style.color = nameColor;
+function cambiaColore() {
+    mioParagrafo.classList.add("textgreen");
+}
