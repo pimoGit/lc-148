2e7fc21dbdbeac4cb8088a3e16a8f7e43346031a
Simone <info@simoneicardi.com>
2025-09-08 11:54:13 +0200

    es. cronometro async

--- js/script.js
+++ js/script.js
@@ -1,10 +1,40 @@
-setTimeout(function () {
-    console.log('Ciao Asincrono');
-}, 0);
+// selezioniamo gli elementi di pagina
+const pageDisplay = document.getElementById("display");
+const startButton = document.querySelector(".btn-success");
+const stopButton = document.querySelector(".btn-danger");
 
-console.log('Ciao');
-console.log('Ancora un evento sincrono');
+// setto variabile di ref al interval
+let timer;
 
+// logica del bottone start
+startButton.addEventListener("click",
+    function () {
 
+        // gestiamo abilità dei bottoni
+        startButton.disabled = true;
+        stopButton.disabled = false;
 
+        // variabile di conteggio
+        let count = 0;
 
+        // incrementiamo il contatore del tempo
+        timer = setInterval(() => {
+            // incremento e output
+            // count++;
+            pageDisplay.innerText = ++count;
+        }, 1000);
+    }
+);
+
+// logica del bottone stop
+stopButton.addEventListener("click",
+    function () {
+
+        // gestiamo abilità dei bottoni
+        startButton.disabled = false;
+        stopButton.disabled = true;
+
+        // concludo l'esecuzione del timer
+        clearInterval(timer);
+    }
+);
