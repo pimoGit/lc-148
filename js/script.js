1813c8ca0fd47b454df484138caddf56b1f1878a
Simone <info@simoneicardi.com>
2025-09-08 12:32:47 +0200

    es. con function nominale

--- js/script.js
+++ js/script.js
@@ -5,40 +5,43 @@ const outputMsg = document.getElementById("message");
 
 
 // logica di gestione click su bottone Upload
-uploadButton.addEventListener("click",
-    function () {
+uploadButton.addEventListener("click", uploada);
 
-        // disabilito il bottone
-        uploadButton.disabled = true;
 
-        // inizializziamo contatore
-        let progress = 0;
-        let progressOutput = progress + "%";
 
-        // usiamo i valori per modificare il DOM
-        progressBar.innerText = progressOutput;
-        progressBar.style.width = progressOutput;
 
-        // simulazione upload (temporale)
-        const progInterval = setInterval(() => {
-            if (progress === 100) {
-                clearInterval(progInterval);
+function uploada() {
 
-                // gestisco messaggio
-                outputMsg.classList.remove("d-none");
-                // tolgo bottone
-                uploadButton.classList.add("d-none");
-            } else {
+    // disabilito il bottone
+    uploadButton.disabled = true;
 
-                // incrementiamo il valore di output
-                progressOutput = ++progress + "%";
+    // inizializziamo contatore
+    let progress = 0;
+    let progressOutput = progress + "%";
 
-                // usiamo i valori per modificare il DOM
-                progressBar.innerText = progressOutput;
-                progressBar.style.width = progressOutput;
-            }
+    // usiamo i valori per modificare il DOM
+    progressBar.innerText = progressOutput;
+    progressBar.style.width = progressOutput;
 
-        }, 100);
+    // simulazione upload (temporale)
+    const progInterval = setInterval(() => {
+        if (progress === 100) {
+            clearInterval(progInterval);
 
-    }
-);
+            // gestisco messaggio
+            outputMsg.classList.remove("d-none");
+            // tolgo bottone
+            uploadButton.classList.add("d-none");
+        } else {
+
+            // incrementiamo il valore di output
+            progressOutput = ++progress + "%";
+
+            // usiamo i valori per modificare il DOM
+            progressBar.innerText = progressOutput;
+            progressBar.style.width = progressOutput;
+        }
+
+    }, 100);
+
+}
