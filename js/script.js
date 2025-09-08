26ab7018a9d45ce702fd3769c748326e1dc330bb
Simone <info@simoneicardi.com>
2025-09-08 12:50:00 +0200

    logica di funzioni per array numeri univoci

--- js/script.js
+++ js/script.js
@@ -1,47 +1,26 @@
-//  selezioniamo gli elemnti di output e interazione dalla page
-const progressBar = document.querySelector(".progress-bar");
-const uploadButton = document.getElementById("upload-button");
-const outputMsg = document.getElementById("message");
-
-
-// logica di gestione click su bottone Upload
-uploadButton.addEventListener("click", uploada);
-
-
-
-
-function uploada() {
-
-    // disabilito il bottone
-    uploadButton.disabled = true;
-
-    // inizializziamo contatore
-    let progress = 0;
-    let progressOutput = progress + "%";
-
-    // usiamo i valori per modificare il DOM
-    progressBar.innerText = progressOutput;
-    progressBar.style.width = progressOutput;
-
-    // simulazione upload (temporale)
-    const progInterval = setInterval(() => {
-        if (progress === 100) {
-            clearInterval(progInterval);
-
-            // gestisco messaggio
-            outputMsg.classList.remove("d-none");
-            // tolgo bottone
-            uploadButton.classList.add("d-none");
-        } else {
-
-            // incrementiamo il valore di output
-            progressOutput = ++progress + "%";
-
-            // usiamo i valori per modificare il DOM
-            progressBar.innerText = progressOutput;
-            progressBar.style.width = progressOutput;
-        }
-
-    }, 100);
-
+function numRandomGen(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+console.log(numRandomGen(10, 50));
+
+
+// funzione che genera un array di tot numeri in un range
+function arrayUniqueNumbers(numMin, numMax, numberElemnts) {
+    // mi creo un array vuoto di partenza
+
+    // ciclo l'array creato FINO CHE è più corto di numberElemnts
+    // genero un numero random in un range
+    let nuovoNumero = numRandomGen(numMin, numMax);
+
+    // verifico se nuovoNumero è già incluse nell'array creato
+    // SE NON è vero
+    // lo pusho nell'array creato
+
+
+    // ritorno l'array creato
+}
+
+
+// arrayUniqueNumbers(10, 50, 5);
