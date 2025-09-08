932cadf5a22e2e9a1bb109b9a96c5d6343711e99
Simone <info@simoneicardi.com>
2025-09-08 11:28:18 +0200

    es. async base JS

--- js/script.js
+++ js/script.js
@@ -1,27 +1,10 @@
-// selezione dell'elemento di output
-const outputDisplay = document.getElementById("display");
-console.log(outputDisplay);
+setTimeout(function () {
+    console.log('Ciao Asincrono');
+}, 0);
 
-// settiamo i valori di partenza
-let seconds = 10;
-outputDisplay.innerText = seconds--;
-
-
-// settiamo l'intervallo in cui deve avvenire il conteggio
-const intervalId = setInterval(function () {
-    // SE sono alla fine 
-    if (seconds === 0) {
-        // allora invio auguri e fermo l'esecuzione
-        outputDisplay.innerText = "Buon annooooo!!!!";
-        clearInterval(intervalId);
-    } else {
-        // ALTRIMENTI
-        // decremento il contatore
-        // e stampo il valore in pagina (output)
-        outputDisplay.innerText = seconds--;
-    }
-
-}, 1000);
+console.log('Ciao');
+console.log('Ancora un evento sincrono');
 
 
-console.log("una cosa sincrona");
+
+
