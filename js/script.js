d0d0aee2a97b272d415a1e8d657c8bb1256fd38d
Simone <info@simoneicardi.com>
2025-09-09 12:11:57 +0200

    es. array di array

--- js/script.js
+++ js/script.js
@@ -1,46 +1,17 @@
-const palla = {
-    "colore": ['red', 'blue'],
-    'tipo': 'pallina da golf',
-    "peso": 40,
-};
+const classi =
 
-const nuovaPalla = {};
+    [
 
-// settaggio di valore esistente
-palla.peso = 50;
-// palla["peso"] = 60;
+        ['Mattia', 'Giacomo'],
+        ['Martina', 'Giorgia']
 
-//creare una nuova prop con valore 
-palla.brand = "newBall";
-// palla["brand"] = "newBall";
+    ];
 
 
-console.log(palla);
-
-
-
-// differenza tra le due notazioni
-/*
-const inputUser = prompt("di quale propietà vuoi il valore?");
-
-
-console.log(palla.inputUser); //'pallina da golf'
-
-
-
-console.log(palla[inputUser]); //'pallina da golf'
-*/
-
-// aggiunta ciclo FOR IN
-
-
-for (let key in palla) {
-    console.log(key, palla[key]);
+for (let i = 0; i < classi.length; i++) {
+    classi[i];
+    for (let j = 0; j < classi[i].length; j++) {
+        classi[i][j]
+    }
 }
 
-
-
-// Risultato atteso: 
-// 12
-// pallina da golf
-// non per forza in questo ordine
