d7b2b27369a3c4e65d9287c0551e5f2e0cadd405
Simone <info@simoneicardi.com>
2025-09-09 12:45:34 +0200

    es. array di oggetti

--- js/script.js
+++ js/script.js
@@ -2,16 +2,36 @@ const classi =
 
     [
 
-        ['Mattia', 'Giacomo'],
-        ['Martina', 'Giorgia']
+        {
+            'nome': 'Classe 1',
+            'numero_studenti': 10
+        },
+
+        {
+            'nome': 'Classe 2',
+            'numero_studenti': 15
+        }
 
     ];
 
 
+// aggiungiamo un elemento
+const nuovaClasse = {
+    'nome': 'Classe 3',
+    'numero_studenti': 20
+};
+
+classi.push(nuovaClasse);
+console.log(classi);
+
+// ciclo la mia lista di classi
 for (let i = 0; i < classi.length; i++) {
-    classi[i];
-    for (let j = 0; j < classi[i].length; j++) {
-        classi[i][j]
+    let classeN = classi[i];
+    // console.log(classeN.numero_studenti);
+
+    for (let nomeProp in classeN) {
+        console.log(nomeProp, classeN[nomeProp]);
     }
+
 }
 
