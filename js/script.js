fed2dca8df692f416b2641c44d0eaa1b31aec665
Simone <info@simoneicardi.com>
2025-09-10 10:16:26 +0200

    costruzione dinamica oggetto

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
-const zucchine = [
-    { varieta: 'Varietà 1', peso: 10, lunghezza: 4 },
-    { varieta: 'Varietà 2', peso: 13, lunghezza: 16 },
-    { varieta: 'Varietà 3', peso: 4, lunghezza: 23 },
-    { varieta: 'Varietà 4', peso: 11, lunghezza: 6 },
-    { varieta: 'Varietà 5', peso: 2, lunghezza: 17 },
-    { varieta: 'Varietà 6', peso: 5, lunghezza: 10 },
-    { varieta: 'Varietà 7', peso: 7, lunghezza: 9 },
-    { varieta: 'Varietà 8', peso: 3, lunghezza: 8 },
-    { varieta: 'Varietà 9', peso: 6, lunghezza: 27 },
-    { varieta: 'Varietà 10', peso: 14, lunghezza: 4 }
-];
+let name = 'Paolo';
+let ageValue = 30; // NOT age
+let email = 'paolo@email.it';
 
-let sommaPesoZucchine = 0;
+const nameKey = 'age'; // Ora qui non c’è ambiguità
 
-for (let i = 0; i < zucchine.length; i++) {
-    const zucchina = zucchine[i];
-    console.log(zucchina.peso);
-    sommaPesoZucchine = sommaPesoZucchine + zucchina.peso;
-}
+const student = {
+    name: name,
+    [nameKey]: ageValue,  // NOT age
+    email: email
+};
 
-console.log("la somma dei pesi è: ", sommaPesoZucchine);
+console.log(student);
