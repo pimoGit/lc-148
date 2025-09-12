028cd9a479e72de1aca0ded77501bcd29c6c641c
Simone <info@simoneicardi.com>
2025-09-12 11:39:08 +0200

    es. utilizzi reali su array di oggetti

--- js/script.js
+++ js/script.js
@@ -1,15 +1,63 @@
-const people = [
-    { id: 4, name: 'Paolo', age: 32 },
-    { id: 3, name: 'Clelia', age: 34 },
+const students = [
+    {
+        id: 1,
+        name: 'Luca Bianchi',
+        age: 32,
+        className: '3C',
+        vote: 9
+    },
+    {
+        id: 2,
+        name: 'Andrea Rossi',
+        age: 18,
+        className: '4A',
+        vote: 7
+    },
+    {
+        id: 3,
+        name: 'Giovanni Verdi',
+        age: 33,
+        className: '5B',
+        vote: 7
+    },
+    {
+        id: 4,
+        name: 'Francesco Esposito',
+        age: 25,
+        className: '5B',
+        vote: 7
+    },
+    {
+        id: 5,
+        name: 'Matteo Ferri',
+        age: 57,
+        className: '5B',
+        vote: 10
+    },
 ];
 
-// trova la person con id 3
-const myPerson = people.find((person) => {
-    return person.id === 3;
+// Stampa in console i nomi di tutti gli studenti
+students.forEach((student) => {
+    console.log(student.name);
 });
 
+// Crea un array con la lista  di tutti i nomi degli studenti
+const studentsName = students.map((student) => {
+    return student.name
+});
 
-console.log(myPerson);
+console.log(studentsName);
 
 
-//RISULTATO: { id: 3, name: 'Clelia', age: 34 }
+// Crea un array che contenga solo gli studenti dai 30 anni in su
+const over30Students = students.filter(student => student.age >= 30);
+
+console.log(over30Students);
+
+
+// Trova lo studente con id 4
+const quartoStudente = students.find((student) => {
+    return student.id === 4;
+});
+
+console.log(quartoStudente);
