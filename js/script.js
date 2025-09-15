cc17aa5bc6fabb929eef40a4c9592b800f90d9ff
Simone <info@simoneicardi.com>
2025-09-15 12:03:41 +0200

    es. prima chiamata ajac con axios

--- js/script.js
+++ js/script.js
@@ -1,63 +1,21 @@
-const students = [
-    {
-        id: 1,
-        name: 'Luca Bianchi',
-        age: 32,
-        className: '3C',
-        vote: 9
-    },
-    {
-        id: 2,
-        name: 'Andrea Rossi',
-        age: 18,
-        className: '4A',
-        vote: 7
-    },
-    {
-        id: 3,
-        name: 'Giovanni Verdi',
-        age: 33,
-        className: '5B',
-        vote: 7
-    },
-    {
-        id: 4,
-        name: 'Francesco Esposito',
-        age: 25,
-        className: '5B',
-        vote: 7
-    },
-    {
-        id: 5,
-        name: 'Matteo Ferri',
-        age: 57,
-        className: '5B',
-        vote: 10
-    },
-];
+// selezioniamo l'elelemnto di output
+const resultOutput = document.getElementById("result");
+const endPointNumber = "https://flynn.boolean.careers/exercises/api/random/int";
 
-// Stampa in console i nomi di tutti gli studenti
-students.forEach((student) => {
-    console.log(student.name);
-});
+// andiamo a fare la chiamata AJAX con axios
+axios.get(endPointNumber)
+    .then(risposta => {
+        //codice da eseguire in caso di successo
+        const result = risposta.data.response;
+        console.log(result);
 
-// Crea un array con la lista  di tutti i nomi degli studenti
-const studentsName = students.map((student) => {
-    return student.name
-});
+        resultOutput.innerText = result;
 
-console.log(studentsName);
+    })
+    .catch(error => {
+        // codice da eseguire in caso di errore
+        console.error(error)
+    })
 
-
-// Crea un array che contenga solo gli studenti dai 30 anni in su
-const over30Students = students.filter(student => student.age >= 30);
-
-console.log(over30Students);
-
-
-// Trova lo studente con id 4
-const quartoStudente = students.find((student) => {
-    return student.id === 4;
-});
-
-console.log(quartoStudente);
+// NON POSSO METTERLO FUORI DAL THEN
+// resultOutput.innerText = result;
