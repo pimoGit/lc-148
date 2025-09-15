81456dfcd79fe51fb9e2a03e6256175046bf6bff
Simone <info@simoneicardi.com>
2025-09-15 12:45:23 +0200

    versione dadi con bottone

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const play1 = document.getElementById("player1Output");
 const play2 = document.getElementById("player2Output");
 const resultOutput = document.getElementById("message");
+// e di interazione
+const gioca = document.getElementById("playbutton");
 
 // settiamo costante per ednpoint di API
 const min = 1;
@@ -9,35 +11,45 @@ const max = 6;
 const nItems = 2;
 const endPointApi = `https://flynn.boolean.careers/exercises/api/array/integers?min=${min}&max=${max}&items=${nItems}`;
 
+
+gioca.addEventListener("click", playDice);
+
+
+
+// FUNZIONE di chiamata per gioco dadi
+function playDice() {
+
+    axios.get(endPointApi)
+        .then(rispApi => {
+            //codice da eseguire in caso di successo
+            const result = rispApi.data.response;
+            console.log(result);
+
+            // estrapolo valori per utilizzo distinto
+            // const playNumber1 = result[0];
+            // const playNumber2 = result[1];
+            const [playNumber1, playNumber2] = result;
+
+            // output dei due numeri
+            play1.innerHTML = `Il nunero uscito per il giocatore Uno è ${playNumber1}`;
+            play2.innerHTML = `Il nunero uscito per il giocatore Due è ${playNumber2}`;
+
+            // costruzione del messaggio finale del risultato
+            let messageOutput = "Avete opareggiato";
+
+            if (playNumber1 > playNumber2) {
+                messageOutput = "Ha vinto il giocatore Uno";
+            } else if (playNumber1 < playNumber2) {
+                messageOutput = "Ha vinto il giocatore Due";
+            }
+
+            resultOutput.innerHTML = messageOutput;
+
+        })
+        .catch(error => {
+            // codice da eseguire in caso di errore
+            console.error(error)
+        })
+
+}
 // chiamata AJAX
-axios.get(endPointApi)
-    .then(rispApi => {
-        //codice da eseguire in caso di successo
-        const result = rispApi.data.response;
-        console.log(result);
-
-        // estrapolo valori per utilizzo distinto
-        // const playNumber1 = result[0];
-        // const playNumber2 = result[1];
-        const [playNumber1, playNumber2] = result;
-
-        // output dei due numeri
-        play1.innerHTML = `Il nunero uscito per il giocatore Uno è ${playNumber1}`;
-        play2.innerHTML = `Il nunero uscito per il giocatore Due è ${playNumber2}`;
-
-        // costruzione del messaggio finale del risultato
-        let messageOutput = "Avete opareggiato";
-
-        if (playNumber1 > playNumber2) {
-            messageOutput = "Ha vinto il giocatore Uno";
-        } else if (playNumber1 < playNumber2) {
-            messageOutput = "Ha vinto il giocatore Due";
-        }
-
-        resultOutput.innerHTML = messageOutput;
-
-    })
-    .catch(error => {
-        // codice da eseguire in caso di errore
-        console.error(error)
-    })
